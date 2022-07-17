5ac7d66504c8b7fc32c02161e9ffe04ab468c7c4
marselich <marsellkaliev7722@gmail.com>
2022-07-17 13:47:31 +0500

    Init project and implemented activity_main.xml

---
+++ app/src/main/java/ru/kalievmars/filewriterreader/MainActivity.java
@@ -0,0 +1,16 @@
+package ru.kalievmars.filewriterreader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private final static String FILE_NAME = "content.txt";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
